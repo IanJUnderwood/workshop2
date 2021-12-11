bbf085753ece5f2664bed78624aaf2b7d2761901
Ian Underwood <ian.underwood28@gmail.com>
2021-12-11 12:05:25 -0500

    oops

--- app/nc_tutorials/settings.py
+++ app/nc_tutorials/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'django-insecure-(j-#aqq@vs$3l#0(031z4_h(_*pa&ye#dt&x69_@(&a6(=+fpg
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['452924950679.dkr.ecr.us-east-2.amazonaws.com/workshop2',
+ALLOWED_HOSTS = ['ec2-3-134-75-237.us-east-2.compute.amazonaws.com',
                  '0.0.0.0', 'localhost', '127.0.0.1']
 
 
